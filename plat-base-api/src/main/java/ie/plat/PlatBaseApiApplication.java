969cd7c2d84210c59c8f1ab758d19a9cdf35399c
mshannon-platoon <platoon.development@gmail.com>
2023-04-28 11:31:03 +0100

    Prop up base Integration tests
    - Added base integration test functionality

--- plat-base-api/src/main/java/ie/plat/PlatBaseApiApplication.java
+++ plat-base-api/src/main/java/ie/plat/PlatBaseApiApplication.java
@@ -2,8 +2,10 @@ package ie.plat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PlatBaseApiApplication {
 
   public static void main(String[] args) {
--- plat-base-api/src/test/integration/java/ie/plat/user/UserRepositoryIT.java
+++ plat-base-api/src/test/integration/java/ie/plat/user/UserRepositoryIT.java
@@ -7,18 +7,14 @@ import ie.plat.PlatBaseApiApplication;
 import ie.plat.util.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PlatBaseApiApplication.class)
-//@AutoConfigureMockMvc
 @ActiveProfiles("integration")
 public class UserRepositoryIT extends BaseIntegrationTest {
 
